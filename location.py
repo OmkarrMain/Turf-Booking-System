6e204052807d9601780642e32b39bbb2f9cc131a
Omkarr <Omkarmain786@gmail.com>
2024-03-05 14:18:23 +0530

    Maps added and sorting also

---
+++ location.py
@@ -0,0 +1,57 @@
+import folium
+import googlemaps
+import webbrowser
+
+gmaps = googlemaps.Client(key='AIzaSyCYcHknfiD3Mj7qm5bKSkxuKFWoUtR71Go')
+
+# Geolocation API to get user's current location
+def get_user_location():
+    try:
+        result = gmaps.geolocate()
+        user_location = (result['location']['lat'], result['location']['lng'])
+        return user_location
+    except Exception as e:
+        print("Error getting user location:", e)
+        return None
+    
+# Places API to search for nearby turfs
+def search_nearby_turfs(user_location):
+    try:
+        places_result = gmaps.places_nearby(location=user_location, radius=5000, keyword='turf')
+        turfs = places_result['results']
+        return turfs
+    except Exception as e:
+        print("Error searching for nearby turfs:", e)
+        return None
+
+# Get user's location
+def main():
+    user_location = get_user_location()
+    if user_location:
+        print("User location:", user_location)
+
+        # Search for nearby turfs
+        turfs = search_nearby_turfs(user_location)
+        if turfs:
+            print("Nearby turfs:")
+            # Create a map centered at the user's location
+            turf_map = folium.Map(location=user_location, zoom_start=15)
+            
+            # Add markers for each nearby turf
+            for turf in turfs:
+                turf_location = (turf['geometry']['location']['lat'], turf['geometry']['location']['lng'])
+                folium.Marker(location=turf_location, popup=turf['name']).add_to(turf_map)
+
+            # Save the map as an HTML file
+            turf_map.save('location.html')
+            print("location.html")
+
+            # Open the generated map in the default web browser
+            webbrowser.open('location.html')
+        else:
+            print("No nearby turfs found.")
+    else:
+        print("Unable to determine user location.")
+
+if __name__ == "__main__":
+    main()
