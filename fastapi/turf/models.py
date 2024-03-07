3112bd20af2a5660cf704735310209ed41e5eb60
Omkarr <Omkarmain786@gmail.com>
2024-03-07 13:22:39 +0530

    fastapi

---
+++ fastapi/turf/models.py
@@ -0,0 +1,18 @@
+#Creating Tables
+
+from sqlalchemy import Boolean, Column, Integer, String
+from database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True)
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(50))
+    content = Column(String(100))
+    user_id = Column(Integer)
+
