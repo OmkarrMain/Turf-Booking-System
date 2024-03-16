14ce9dd096417cba24d35e033b00c77354118b70
Omkarr <Omkarmain786@gmail.com>
2024-03-17 02:56:35 +0530

    cricket page done

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,44 +1,96 @@
-// JavaScript to toggle filter options container
-document
-.getElementById("filterButton")
-.addEventListener("click", function () {
-  var filterOptionsContainer = document.querySelector(
-    ".filterOptionsContainer"
-  );
-  filterOptionsContainer.style.display =
-    filterOptionsContainer.style.display === "block" ? "none" : "block";
+// JavaScript for dashboard functionality
+
+// Function to toggle filter options container
+document.getElementById("filterButton").addEventListener("click", function (event) {
+  var filterOptionsContainer = document.querySelector(".filterOptionsContainer");
+  filterOptionsContainer.style.display = filterOptionsContainer.style.display === "block" ? "none" : "block";
+  event.stopPropagation(); // Prevent event propagation to avoid immediate closing
 });
 
-// Add event listener for more options button
-document
-.querySelectorAll(".moreOptionsButton")
-.forEach(function (button) {
-  button.addEventListener("click", function () {
+// Add event listener for more options buttons
+document.querySelectorAll(".moreOptionsButton").forEach(function (button) {
+  button.addEventListener("click", function (event) {
     var container = this.closest(".container");
     var floatingButtons = container.querySelector(".floatingButtons");
-    floatingButtons.style.display =
-      floatingButtons.style.display === "block" ? "none" : "block";
+    floatingButtons.style.display = floatingButtons.style.display === "block" ? "none" : "block";
+    event.stopPropagation(); // Prevent event propagation to avoid immediate closing
   });
 });
 
 // Add event listener for back button
-document
-.getElementById("backButton")
-.addEventListener("click", function () {
+document.getElementById("backButton").addEventListener("click", function () {
   // Redirect to home page
-  window.location.href = "/"; // Change "home.html" to your actual home page URL
+  window.location.href = "/"; // Change to your actual home page URL
 });
 
 // Function to filter turfs based on sport
 function filterTurf(sport) {
-var containers = document.querySelectorAll(".container");
-containers.forEach(function (container) {
-  var smallContainer = container.querySelector(".smallContainer");
-  var dataText = smallContainer.getAttribute("data-text");
-  if (dataText !== sport) {
-    container.style.display = "none"; // Hide containers that don't match the selected sport
-  } else {
-    container.style.display = "block"; // Show containers that match the selected sport
-  }
-});
+  var containers = document.querySelectorAll(".container");
+  containers.forEach(function (container) {
+    var smallContainer = container.querySelector(".smallContainer");
+    var dataText = smallContainer.getAttribute("data-text");
+    var hourlyPrice;
+    var openingHours;
+
+    // Additional information based on the selected sport
+    switch (sport) {
+      case "Football":
+        hourlyPrice = "₹20";
+        openingHours = "9:00 AM - 10:00 PM";
+        break;
+      case "Cricket":
+        hourlyPrice = "₹15";
+        openingHours = "10:00 AM - 8:00 PM";
+        break;
+      case "Tennis":
+        hourlyPrice = "₹25";
+        openingHours = "8:00 AM - 11:00 PM";
+        break;
+      case "Badminton":
+        hourlyPrice = "₹10";
+        openingHours = "11:00 AM - 9:00 PM";
+        break;
+      case "Yoga":
+        hourlyPrice = "₹12";
+        openingHours = "7:00 AM - 9:00 PM";
+        break;
+      case "Swimming":
+        hourlyPrice = "₹30";
+        openingHours = "6:00 AM - 12:00 AM";
+        break;
+      case "Rugby":
+        hourlyPrice = "₹18";
+        openingHours = "12:00 PM - 8:00 PM";
+        break;
+      case "Hockey":
+        hourlyPrice = "₹22";
+        openingHours = "3:00 PM - 10:00 PM";
+        break;
+      default:
+        break;
+    }
+
+    // Update container content if it matches the selected sport
+    if (dataText === sport) {
+      container.style.display = "block"; // Show containers that match the selected sport
+
+      // Add heading and additional information
+      container.innerHTML = `
+        <h1>${sport}</h1>
+        <div class="smallContainer" style="background-image: ${smallContainer.style.backgroundImage}"></div>
+        <p>Hourly Price: ${hourlyPrice}</p>
+        <p>Opening Hours: ${openingHours}</p>
+      `;
+    } else {
+      container.style.display = "none"; // Hide containers that don't match the selected sport
+    }
+  });
 }
+
+// Function to close floating buttons when clicking anywhere on the page
+document.addEventListener("click", function () {
+  document.querySelectorAll(".floatingButtons").forEach(function (floatingButtons) {
+    floatingButtons.style.display = "none";
+  });
+  document.querySelector(".filterOptionsContainer").style.display = "none"; // Close filter options container
+});
--- static/js/main.js
+++ static/js/main.js
@@ -16,14 +16,14 @@ function promptForLocation() {
 function detectUserLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
-            function(position) {
+            function (position) {
                 var latitude = position.coords.latitude;
                 var longitude = position.coords.longitude;
                 var locationText = "Latitude: " + latitude + ", Longitude: " + longitude;
                 // Update location in navigation bar
                 document.getElementById('location').innerText = locationText;
             },
-            function(error) {
+            function (error) {
                 console.error('Error getting user location:', error);
             }
         );
@@ -33,7 +33,7 @@ function detectUserLocation() {
 }
 
 // Event listener for page load
-window.onload = function() {
+window.onload = function () {
     promptForLocation();
 };
 
@@ -87,41 +87,41 @@ function sendOTP() {
     const email = document.getElementById('email');
     document.write(email.value)
     const otpverify = document.getElementsByClassName('otpverify')[0];
-  
+
     let otp_val = Math.floor(Math.random() * 10000);
-  
+
     let emailbody = `<h2>Your OTP is </h2>${otp_val}`;
     Email.send({
-      SecureToken: "6a7d1bc0-e16a-4997-abbe-bd6242a15c87",
-      To: email.value,
-      From: "omkarmain786@gmail.com",
-      Subject: "Email OTP Verification Code",
-      Body: emailbody,
+        SecureToken: "6a7d1bc0-e16a-4997-abbe-bd6242a15c87",
+        To: email.value,
+        From: "omkarmain786@gmail.com",
+        Subject: "Email OTP Verification Code",
+        Body: emailbody,
     }).then(
-      message => {
-        if (message === "OK") {
-          alert("OTP sent to your email " + email.value);
-  
-          otpverify.style.display = "flex";
-          const otp_inp = document.getElementById('otp_inp');
-          const otp_btn = document.getElementById('otp-btn');
-  
-          otp_btn.addEventListener('click', () => {
-            if (otp_inp.value == otp_val) {
-              alert("Email address verified...");
-              window.location.href = "index.html"; // This will Redirect to index.html 
-            } else {
-              alert("Invalid OTP");
-            }
-          })
-        }
-      }
-    );
-  }
-  
+        message => {
+            if (message === "OK") {
+                alert("OTP sent to your email " + email.value);
 
-  // SIgninreg.js
-  function signup(){
+                otpverify.style.display = "flex";
+                const otp_inp = document.getElementById('otp_inp');
+                const otp_btn = document.getElementById('otp-btn');
+
+                otp_btn.addEventListener('click', () => {
+                    if (otp_inp.value == otp_val) {
+                        alert("Email address verified...");
+                        window.location.href = "index.html"; // This will Redirect to index.html 
+                    } else {
+                        alert("Invalid OTP");
+                    }
+                })
+            }
+        }
+    );
+}
+
+
+// SIgninreg.js
+function signup() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var user_password = document.getElementById("password").value;
@@ -139,38 +139,38 @@ function createUser(name, email, password) {
             user_password: password
         })
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Failed to create user');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('User created successfully:', data);
-        // Handle success response here
-    })
-    .catch(error => {
-        console.error('Error creating user:', error);
-        // Handle error response here
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to create user');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('User created successfully:', data);
+            // Handle success response here
+        })
+        .catch(error => {
+            console.error('Error creating user:', error);
+            // Handle error response here
+        });
 }
 
 // Function to read a user by ID
 function readUser(userId) {
     fetch(`/users/${userId}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Failed to fetch user');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('User:', data);
-        // Display user data
-        document.getElementById('readResults').innerHTML = JSON.stringify(data);
-    })
-    .catch(error => {
-        console.error('Error fetching user:', error);
-        // Handle error response here
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch user');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('User:', data);
+            // Display user data
+            document.getElementById('readResults').innerHTML = JSON.stringify(data);
+        })
+        .catch(error => {
+            console.error('Error fetching user:', error);
+            // Handle error response here
+        });
 }
