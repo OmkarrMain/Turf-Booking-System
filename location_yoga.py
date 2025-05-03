ad7837e33fd25692242ad06e472cddfef1c3b8f0
Omkarr <omkarmain786@gmail.com>
2025-05-03 20:19:43 +0530

    Update location_yoga.py

--- location_yoga.py
+++ location_yoga.py
@@ -3,7 +3,7 @@ import googlemaps
 from jinja2 import Environment, FileSystemLoader
 import random
 
-gmaps = googlemaps.Client(key='AIzaSyCYcHknfiD3Mj7qm5bKSkxuKFWoUtR71Go')
+gmaps = googlemaps.Client(key='')
 
 def get_user_location():
     try:
